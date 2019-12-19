a53a7e614ec4c7c0158f1ed7e0bb432ed5f0e46a
AyamDobhal <dobhalayam@gmail.com>
2019-12-19 13:44:15 +0530

    created check for creds

--- main.py
+++ main.py
@@ -1,7 +1,23 @@
 import praw
 import json
 from getpass import getpass
+import pathlib, os
 
+
+def banner():
+	print('''
+	██████╗ ███████╗██████╗ ██████╗ ██╗████████╗    ██████╗██╗     ██╗
+	██╔══██╗██╔════╝██╔══██╗██╔══██╗██║╚══██╔══╝   ██╔════╝██║     ██║S
+	██████╔╝█████╗  ██║  ██║██║  ██║██║   ██║█████╗██║     ██║     ██║
+	██╔══██╗██╔══╝  ██║  ██║██║  ██║██║   ██║╚════╝██║     ██║     ██║
+	██║  ██║███████╗██████╔╝██████╔╝██║   ██║      ╚██████╗███████╗██║
+	╚═╝  ╚═╝╚══════╝╚═════╝ ╚═════╝ ╚═╝   ╚═╝       ╚═════╝╚══════╝╚═╝
+	''')
+	print('''\t\t\t\t\tby /u/AyamDobhal and /u/VineetAhujaX\n\n\n''')
+
+def cred_check():
+	creds = pathlib.Path.cwd() /'credentials.json'
+	return creds.exists()
 
 def authenticate():
 	with open('credentials.json') as creds:
@@ -9,8 +25,18 @@ def authenticate():
 
 	reddit = praw.Reddit(client_id = credentials['client_id'],
 				client_secret = credentials['client_secret'],
-				user_agent = credentials['user_agent']
-				username = input('Enter your reddit username: ')
+				user_agent = credentials['user_agent'],
+				username = input('Enter your reddit username: '),
 				password = getpass('Enter your reddit password: ')
 			)
 	return reddit
+
+def main():
+	banner()
+	if cred_check() != True:
+		print('''credentials.txt does not exist. Refer to the instructions in README.md to create it. 
+		reddit-cli will exit now.''')
+		os.sys.exit()
+	REDDIT = authenticate()
+while __name__ == '__main__':
+	main()
