9ce8844999d8657d5abe0bef657b548fbb353900
AyamDobhal <dobhalayam@gmail.com>
2019-12-20 11:21:16 +0530

    fixed the main import

--- subreddit/scraper.py
+++ subreddit/scraper.py
@@ -1,4 +1,7 @@
-import praw, json, os
+import praw, json, os, sys
+#to add the parent directory to the system path.
+sys.path.append('.')
+
 from main import REDDIT
 
 with open('credentials.txt') as creds:
