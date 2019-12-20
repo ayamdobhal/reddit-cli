fce5bf640c23223006093e12ae788ba142c266d7
AyamDobhal <dobhalayam@gmail.com>
2019-12-20 14:50:31 +0530

    completed the get_post function

--- subreddit/scraper.py
+++ subreddit/scraper.py
@@ -1,12 +1,35 @@
-import praw, json, os, sys
+import os, sys, time
+import webbrowser
+import prawcore
+from contextlib import suppress
+
 #to add the parent directory to the system path.
 sys.path.append('.')
 
-from main import REDDIT
+import praw
+import main
 
-with open('credentials.txt') as creds:
-    credentials = json.load(creds)
-
-def get_hot(subreddit):
-    subreddit = REDDIT.subreddit(subreddit)
+def get_hot(reddit):
+    main.clrscr()
+    main.banner()
+    subreddit = input('Enter the name of the subreddit you want to fetch submissions from:')
+    try:
+        subreddit = reddit.subreddit(subreddit)
+        print('\nFetching hot submissions from %s...\n\n'%(subreddit.title))
+        submissions = []
+        for submission in subreddit.hot(limit=10):
+            print(submission.title, 'id=%s'%(submission.id))
+            print('score=%s'%(submission.score))
+            choice = input('''Do you want to open this submission in your browser?
+                         Y for yes, Q to go back to menu or any other key to skip to next submission\n''')
+            print('--------------------------------------------------------------------------------------------')
+            if choice.lower() == 'y':
+                webbrowser.open_new_tab('reddit.com/r/%s/comments/%s'%(subreddit,submission.id))
+            elif choice.lower() == 'q':
+                break
+    except:
+        print('subreddit does not exist. \nReturning to menu in 2 seconds...')
+        time.sleep(2)
+        suppress(Exception)
+        
     
