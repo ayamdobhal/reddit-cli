6b351a6025dd779d4db444e2a5cc2b9901d0b574
Ayam Dobhal <dobhalayam@gmail.com>
2020-02-07 09:49:33 +0530

    revert 004654a6ce4441e3e62d7f05f9fb27bfaf60a30d

--- reddit/modules/login.py
+++ reddit/modules/login.py
@@ -10,7 +10,6 @@ import json, os, pathlib
 import praw
 from stdiomask import getpass
 import urllib.request
-from hashlib import md5
 
 
 def cred_check():
@@ -37,27 +36,14 @@ def authenticate():
     try:
         with open("credentials.json") as creds:
             credentials = json.load(creds)
-        try:
-            with open('login.txt') as login:
-                log = login.readlines()
-                usrname = log[0]
-                pwd = log[1]
-        except:
-            usrname = input('Enter your username: ')
-            password = getpass('Enter your password: ')
-            choice = input("Do you want to save your login information?[Y for yes]")
-            if choice.lower() == 'y':
-                with open('login.txt','w') as login:
-                    login.write(usrname+'\n'+pwd)
-        
+
         reddit = praw.Reddit(
             client_id=credentials["client_id"],
             client_secret=credentials["client_secret"],
             user_agent=credentials["user_agent"],
-            username = usrname,
-            password = pwd
+            username=input("Enter your reddit username: "),
+            password=getpass("Enter your reddit password: "),
         )
-        
         if reddit.user.me() == None:
             os.sys.exit()
         print("Welcome /u/%s!\n\n" % (reddit.user.me()))
